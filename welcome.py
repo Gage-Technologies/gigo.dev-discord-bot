23ba96025c32fe80a7c070d3821dda0c00f50b0f
Sam Ulrich <40002776+sam-ulrich1@users.noreply.github.com>
2023-10-20 14:06:41 -0500

    Switch to DMs for new members

--- welcome.py
+++ welcome.py
@@ -1,7 +1,8 @@
 # Handle welcome behavior for users
 
+from ast import For
 import discord
-from discord import TextChannel, Member, PermissionOverwrite, Guild
+from discord import Member, Forbidden
 
 
 WELCOME_MESSAGE = """Welcome to the server, {username}!
@@ -16,36 +17,8 @@ async def on_member_join_handler(member: Member) -> None:
     """
     Says hello to a new user
     """
-    # get the welcome channel
-    channel: TextChannel = discord.utils.get(
-        member.guild.text_channels, name='welcome'
-    )
-
-    # exit if no channel
-    if not channel:
-        return
-
-    # create the permission overwrites
-    overwrites = {
-        member.guild.default_role: PermissionOverwrite(read_messages=False),
-        member: PermissionOverwrite(read_messages=True),
-    }
-
-    # retrieve the admin roles
-    dev_role = discord.utils.get(member.guild.roles, name='dev')
-    not_dev_role = discord.utils.get(member.guild.roles, name='not dev')
-
-    # remove dev and not_dev from the list of roles
-    if dev_role:
-        overwrites[dev_role] = PermissionOverwrite(read_messages=False)
-    if not_dev_role:
-        overwrites[not_dev_role] = PermissionOverwrite(read_messages=False)
-
-    # create the private channel to message the user
-    new_channel: TextChannel = await member.guild.create_text_channel(
-        f"gigo-bot-{member.id}",
-        overwrites=overwrites,
-    )
-
     # send the welcome message
-    await new_channel.send(WELCOME_MESSAGE.format(username=member.mention))
+    try:
+        await member.send(WELCOME_MESSAGE.format(username=member.mention))
+    except Forbidden:
+        print(f"{member}'s DMs are locked; failed to welcome them")
